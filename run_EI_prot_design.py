c2efbd60009f9173eca4a1647e16ebd98142b237
Daryl <daryl.barth@utexas.edu>
2024-08-05 15:22:48 -0500

    Set up scripts structure for the project

---
+++ run_EI_prot_design.py
@@ -0,0 +1,4 @@
+#Make MSA with make_msa.sh
+#Calculate conserved residues with different thresholds with calc_conserved_residues.py
+#Use the conserved residues to make a freesing hash for LigandMPNN
+#Run LigandMPNN on the ligand dataset to get your Evolutionarily Informed Protein Design Variants 
